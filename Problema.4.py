de75a763323dab86718f57cd4baf038314f3452f
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-09-22 22:54:36 +0300

    Add files via upload

---
+++ Problema.4.py
@@ -0,0 +1,7 @@
+print ("Problema 4")
+a=2
+print ("2m =" , a*100 , "cm")
+print ("2kg =" , a*(10**6) , "mg")
+print ("2ani =" , a*12 , "luni" )
+print ("2ani =" , a*52.17 , "saptamini" )
+print ("2ani =" , a*365.25 , "zile" )
