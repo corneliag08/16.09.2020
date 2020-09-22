82f1b20b145f9d6764c16aa08aa212a0ec70827c
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-09-22 22:34:53 +0300

    Add files via upload

---
+++ Problema.3.py
@@ -0,0 +1,14 @@
+print ("Problema 3")
+a=3
+b=6
+c=4
+d=9
+print ("Se dă:")
+print ("a=3")
+print ("P=?")
+print ("At=?")
+print ("V=?")
+print ("Rezolvare:")
+print ("P=" , b*a*c )
+print ("At=" , b*d )
+print ("V=" ,a*a*a )
