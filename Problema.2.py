74de2e9736ded26fad649c6829b9b39054f4cb96
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-09-22 21:46:25 +0300

    Add files via upload

---
+++ Problema.2.py
@@ -0,0 +1,11 @@
+print ("Problema 2")
+a=8
+b=2
+print ("a=8")
+print ("b=2")
+print (a , "+" , b , "=" , a+b)
+print (a , "-" , b , "=" , a-b)
+print (a , "//" , b , "=" , a//b)
+print (a , "%" , b , "=" , a%b)
+print (a , "*" , b , "=" , a*b)
+print (a , "**" , b , "=" , a**b)
